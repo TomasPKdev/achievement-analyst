ac1b516d874152f871cc63d1a3b6c91d1ddc46a9
TomasPKdev <tomaspk.dev@gmail.com>
2026-09-07 21:39:54 -0300

    Add debounced game search with autocomplete suggestions

--- script-analyst.js
+++ script-analyst.js
@@ -1,8 +1,20 @@
 //script.js
 const form = document.getElementById('search-form'); //requiero el form pq es el q manda el submit
 const input = document.getElementById('game-input');//el input este es el texto q escribis en la caja de input
+let timer;
 
-form.addEventListener('submit', function(event) { //la constante form le creo un evento q escucha cuando form se hace submit, y cuando pase eso, corre una funcion con el parametro event
-  event.preventDefault(); // este parametro event solo hace q la pagina no se recargue cuando le das submit al form
-  console.log(input.value); // muesta el resultado por consola
+form.addEventListener('input', function(event) { //la constante form le creo un evento q escucha cuando form ve q input tiene algo, y cuando pase eso, corre una funcion con el parametro event
+  clearTimeout(timer);
+  timer = setTimeout(function() {
+  fetch(`https://store.steampowered.com/api/storesearch/?term=${input.value}&l=english&cc=us`) //fetch hace un pedido http a ese link y la convierte en promesa
+  .then(response => response.json()) //esta promesa se transforma en la respuesta y se convierte en json
+  .then(data => {
+  for (let i = 0; i < 4; i++) {
+    const game = data.items[i];
+    const article = document.getElementById('suggestion' + (i + 1));
+    article.querySelector('img').src = game.tiny_image;
+    article.querySelector('p').textContent = game.name;
+  }
+});
+}, 400); //este timeout hace q no se haga el fetch hasta q el usuario deje de escribir por 400ms
 });
